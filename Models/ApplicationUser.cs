335b0d7865b91df7ae2789b2f3ecba78ff16f72e
Cheng <dongdon0639@gmail.com>
2018-06-28 13:49:17 +0800

    Add ApplicationUser class that implement IdentityUser.

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace mvc.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public ApplicationUser()
+        {
+        }
+
+        public ApplicationUser(string userName) : base(userName)
+        {
+        }
+
+    }
+}
