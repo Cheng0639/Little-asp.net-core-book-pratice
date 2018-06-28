aaa2e59a072d000f01e30e63b42be7f29ae076c4
Cheng <dongdon0639@gmail.com>
2018-06-28 16:25:02 +0800

    Change interface of ITodoItemService.

--- Service/ITodoItemService.cs
+++ Service/ITodoItemService.cs
@@ -1,16 +1,17 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using mvc.Models;
 
 namespace mvc.Service
 {
     public interface ITodoItemService
     {
-        Task<IEnumerable<TodoItem>> GetInCompleteItemsAsync(ApplicationUser user);
+        Task<IEnumerable<TodoItem>> GetInCompleteItemsAsync(IdentityUser user);
 
-        Task<bool> AddItemAsync(TodoItem newItem, ApplicationUser user);
+        Task<bool> AddItemAsync(TodoItem newItem, IdentityUser user);
 
-        Task<bool> MarkDoneAsync(Guid id, ApplicationUser user);
+        Task<bool> MarkDoneAsync(Guid id, IdentityUser user);
     }
 }
